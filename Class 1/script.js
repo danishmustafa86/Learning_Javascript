6908b08dc8efc0358327433627010b9a0665c457
Danish jajja <danishjajja86@gmail.com>
2023-06-10 12:43:44 +0500

    Class 1

--- Class 1/script.js
+++ Class 1/script.js
@@ -1,2 +1,9 @@
-let x = 5;
-console.log(x);
+// practice for variable
+// = assignment operator for assigning value to variable
+// keyword or word,
+//we saved some variables in RAM
+var age = 20
+var heigth = 5.5
+var name = "naveed sarwar"
+
+//how to get these variables
