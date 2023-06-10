d1e0574b7f105c08f84c533eb7014f339bf11926
Danish jajja <danishjajja86@gmail.com>
2023-06-10 17:55:36 +0500

    question no 4

---
+++ Assignment 1/script.js
@@ -0,0 +1,36 @@
+// Question-03
+
+// Write a js program to find maximum between two numbers.
+
+// var num1 = 4
+// var num2 = 5
+// if(num1>num2)
+//     console.log(num1)
+// else
+//     console.log(num2)
+
+
+// Question-04
+
+//  Write a js program to find maximum between three numbers.
+
+var n1 =3
+var n2 = 4
+var n3=7
+if(n1>n2)
+
+   if(n1>n3)
+   console.log(n1)
+   else
+   console.log(n3)
+else
+   if(n2>n3)
+   console.log(n2)
+   else
+   console.log(n3)
+
+
+
+console.log()
+
+    
