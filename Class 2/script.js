65286e13838b6d784e03df0cc4de8b50b7fdb370
Danish jajja <danishjajja86@gmail.com>
2023-06-10 12:42:14 +0500

    class 2 going on

---
+++ Class 2/script.js
@@ -0,0 +1,9 @@
+// practice for variable
+// = means assignment operator
+// keywords 
+var age = 20
+var height = 5.5
+var name = "naveed sarwar"
+//HOW TO GET these variables from RAM?
+console.log(age);
+
