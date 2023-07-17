783db7d7b226a0ed09e0ca7f76f789473fda8ec6
Danish jajja <danishjajja86@gmail.com>
2023-07-17 12:18:56 +0500

    pro 6

--- Assignment 1/pro 3/script.js
+++ Assignment 1/pro 3/script.js
@@ -1,36 +1,46 @@
-// // // var n1 = 54
-// // // var n2 = 34
-// // // if (n1 > n2) {
-// // //   console.log('n1 is maximum')
-// // // } else {
-// // //   console.log('n2 is maximum')
+// // // // var n1 = 54
+// // // // var n2 = 34
+// // // // if (n1 > n2) {
+// // // //   console.log('n1 is maximum')
+// // // // } else {
+// // // //   console.log('n2 is maximum')
+// // // // }
+// // // var n1 = 5
+// // // var n2 = 8
+// // // var n3 = 4
+// // // if(n1>n2&&n1>n3)
+// // // {
+// // //     console.log("n1 is greatear")
 // // // }
-// // var n1 = 5
-// // var n2 = 8
-// // var n3 = 4
-// // if(n1>n2&&n1>n3)
-// // {
-// //     console.log("n1 is greatear")
-// // }
-// // if(n2>n3&&n2>n1)
-// // {
-// //     console.log("n2 is greater")
-// // }
-// // if(n3>n2&&n3>n1)
-// // {
-// // console.log("n3 is greater")
-// // }
-// var num = 3
-// if(num>0)
-// console.log("num is positive")
-// else if (num<0)
-// console.log("num is nagative")
+// // // if(n2>n3&&n2>n1)
+// // // {
+// // //     console.log("n2 is greater")
+// // // }
+// // // if(n3>n2&&n3>n1)
+// // // {
+// // // console.log("n3 is greater")
+// // // }
+// // var num = 3
+// // if(num>0)
+// // console.log("num is positive")
+// // else if (num<0)
+// // console.log("num is nagative")
+// // else
+// // console.log("num is equal to zer
+// var n = 5
+// if(n%2==0)
+// console.log("n is eveen number")
 // else
-// console.log("num is equal to zer
-var n = 5
-if(n%2==0)
-console.log("n is eveen number")
+// console.log("n is oddd number")
+var n = 55
+if(n/5&&n/11)
+{
+    console.log("n is divisabie by 5 and 11")
+}
 else
-console.log("n is oddd number")
+{
+console.log ("invalid input")
+}
+
 
 
