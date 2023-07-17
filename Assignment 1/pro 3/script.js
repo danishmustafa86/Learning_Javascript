661f87845f61eb2da711aa30cc1fdde536f67b54
Danish jajja <danishjajja86@gmail.com>
2023-07-17 11:46:59 +0500

    pro 4

--- Assignment 1/pro 3/script.js
+++ Assignment 1/pro 3/script.js
@@ -1,7 +1,22 @@
-var n1 = 54
-var n2 = 34
-if (n1 > n2) {
-  console.log('n1 is maximum')
-} else {
-  console.log('n2 is maximum')
+// var n1 = 54
+// var n2 = 34
+// if (n1 > n2) {
+//   console.log('n1 is maximum')
+// } else {
+//   console.log('n2 is maximum')
+// }
+var n1 = 5
+var n2 = 8
+var n3 = 4
+if(n1>n2&&n1>n3)
+{
+    console.log("n1 is greatear")
+}
+if(n2>n3&&n2>n1)
+{
+    console.log("n2 is greater")
+}
+if(n3>n2&&n3>n1)
+{
+console.log("n3 is greater")
 }
