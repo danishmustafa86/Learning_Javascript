663440b6f9b0cd353ee85326459d359de409c028
Danish jajja <danishjajja86@gmail.com>
2023-07-17 10:05:39 +0500

    programming

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,36 +1,12 @@
-// Question-03
-
-// Write a js program to find maximum between two numbers.
-
-// var num1 = 4
-// var num2 = 5
-// if(num1>num2)
-//     console.log(num1)
-// else
-//     console.log(num2)
 
 
-// Question-04
+// Question-05
 
-//  Write a js program to find maximum between three numbers.
-
-var n1 =3
-var n2 = 4
-var n3=7
-if(n1>n2)
-
-   if(n1>n3)
-   console.log(n1)
-   else
-   console.log(n3)
+// Write a js program to check whether a number is negative, positive or zero.
+var n = -56
+if(n>0)
+console.log("n is positive");
+else if(n<0)
+console.log("n is negative");
 else
-   if(n2>n3)
-   console.log(n2)
-   else
-   console.log(n3)
-
-
-
-console.log()
-
-    
+console.log("n is equal to zero");
