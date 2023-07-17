efa18197692a15674ff5e5a1db1171721db9b7be
Danish jajja <danishjajja86@gmail.com>
2023-07-17 11:56:36 +0500

    pro 5

--- Assignment 1/pro 3/script.js
+++ Assignment 1/pro 3/script.js
@@ -1,22 +1,30 @@
-// var n1 = 54
-// var n2 = 34
-// if (n1 > n2) {
-//   console.log('n1 is maximum')
-// } else {
-//   console.log('n2 is maximum')
+// // var n1 = 54
+// // var n2 = 34
+// // if (n1 > n2) {
+// //   console.log('n1 is maximum')
+// // } else {
+// //   console.log('n2 is maximum')
+// // }
+// var n1 = 5
+// var n2 = 8
+// var n3 = 4
+// if(n1>n2&&n1>n3)
+// {
+//     console.log("n1 is greatear")
 // }
-var n1 = 5
-var n2 = 8
-var n3 = 4
-if(n1>n2&&n1>n3)
-{
-    console.log("n1 is greatear")
-}
-if(n2>n3&&n2>n1)
-{
-    console.log("n2 is greater")
-}
-if(n3>n2&&n3>n1)
-{
-console.log("n3 is greater")
-}
+// if(n2>n3&&n2>n1)
+// {
+//     console.log("n2 is greater")
+// }
+// if(n3>n2&&n3>n1)
+// {
+// console.log("n3 is greater")
+// }
+var num = 3
+if(num>0)
+console.log("num is positive")
+else if (num<0)
+console.log("num is nagative")
+else
+console.log("num is equal to zero")
+
