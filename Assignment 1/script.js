bc519b03156178a1bbbbcddbc25502da94510072
Danish jajja <danishjajja86@gmail.com>
2023-07-18 16:26:47 +0500

    pro no 5

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,12 +1,26 @@
 
 
-// Question-05
+// // Question-05
 
-// Write a js program to check whether a number is negative, positive or zero.
-var n = -56
-if(n>0)
-console.log("n is positive");
-else if(n<0)
-console.log("n is negative");
-else
-console.log("n is equal to zero");
+// // Write a js program to check whether a number is negative, positive or zero.
+// var n = -56
+// if(n>0)
+// console.log("n is pitive");
+// else if(n<0)
+// console.log("n is negative");
+// else
+// console.log("n is equal to zero");
+// write a program to check a character is an alphabet or not
+var ch = 'a'
+if(ch=='a'||ch=='A'||ch=='b'||ch=='B'||ch=='c'||ch=='C'||ch=='d'||ch=='D'||ch=='e'||ch=='E'||ch=='f'||ch=='F'||
+    ch=='g'||ch=='G'||ch=='h'||ch=='H'||ch=='i'||ch=='I'||ch=='j'||ch=='J'||ch=='k'||ch=='K'||ch=='l'||ch=='L'||
+    ch=='m'||ch=='M'||ch=='n'||ch=='N'||ch=='o'||ch=='O'||ch=='p'||ch=='P'||ch=='q'||ch=='Q'||ch=='r'||ch=='R'||
+    ch=='s'||ch=='S'||ch=='t'||ch=='T'||ch=='u'||ch=='U'||ch=='v'||ch=='V'||ch=='w'||ch=='W'||ch=='x'||ch=='X'||
+    ch=='y'||ch=='Y'||ch=='z'||ch=='Z')
+    {
+        console.log("given character is an alphabet")
+    }
+    else
+    {
+    console.log("it is not an alphabet")
+    }
