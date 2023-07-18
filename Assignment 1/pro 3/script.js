041ded2b8c9ccf4e3ede35213a14193db38791a5
Danish jajja <danishjajja86@gmail.com>
2023-07-18 22:28:48 +0500

    test

--- Assignment 1/pro 3/script.js
+++ Assignment 1/pro 3/script.js
@@ -1,65 +1,69 @@
-// // // // // // // var n1 = 54
-// // // // // // // var n2 = 34
-// // // // // // // if (n1 > n2) {
-// // // // // // //   console.log('n1 is maximum')
-// // // // // // // } else {
-// // // // // // //   console.log('n2 is maximum')
+// // // // // // // // var n1 = 54
+// // // // // // // // var n2 = 34
+// // // // // // // // if (n1 > n2) {
+// // // // // // // //   console.log('n1 is maximum')
+// // // // // // // // } else {
+// // // // // // // //   console.log('n2 is maximum')
+// // // // // // // // }
+// // // // // // // var n1 = 5
+// // // // // // // var n2 = 8
+// // // // // // // var n3 = 4
+// // // // // // // if(n1>n2&&n1>n3)
+// // // // // // // {
+// // // // // // //     console.log("n1 is greatear")
 // // // // // // // }
-// // // // // // var n1 = 5
-// // // // // // var n2 = 8
-// // // // // // var n3 = 4
-// // // // // // if(n1>n2&&n1>n3)
-// // // // // // {
-// // // // // //     console.log("n1 is greatear")
-// // // // // // }
-// // // // // // if(n2>n3&&n2>n1)
-// // // // // // {
-// // // // // //     console.log("n2 is greater")
-// // // // // // }
-// // // // // // if(n3>n2&&n3>n1)
-// // // // // // {
-// // // // // // console.log("n3 is greater")
-// // // // // // }
-// // // // // var num = 3
-// // // // // if(num>0)
-// // // // // console.log("num is positive")
-// // // // // else if (num<0)
-// // // // // console.log("num is nagative")
+// // // // // // // if(n2>n3&&n2>n1)
+// // // // // // // {
+// // // // // // //     console.log("n2 is greater")
+// // // // // // // }
+// // // // // // // if(n3>n2&&n3>n1)
+// // // // // // // {
+// // // // // // // console.log("n3 is greater")
+// // // // // // // }
+// // // // // // var num = 3
+// // // // // // if(num>0)
+// // // // // // console.log("num is positive")
+// // // // // // else if (num<0)
+// // // // // // console.log("num is nagative")
+// // // // // // else
+// // // // // // console.log("num is equal to zer
+// // // // // var n = 5
+// // // // // if(n%2==0)
+// // // // // console.log("n is number")
 // // // // // else
-// // // // // console.log("num is equal to zer
-// // // // var n = 5
-// // // // if(n%2==0)
-// // // // console.log("n is number")
+// // // // // console.log("n is oddd r")
+// // // // var n = 55
+// // // // if(n/5&&n/11)
+// // // // {
+// // // //     console.log("n is divisabie by 5 and 786")
+// // // // }
 // // // // else
-// // // // console.log("n is oddd r")
-// // // var n = 55
-// // // if(n/5&&n/11)
-// // // {
-// // //     console.log("n is divisabie by 5 and 786")
-// // // }
-// // // else
-// // // {
-// // // console.log ("invalid input")
-// // // }
+// // // // {
+// // // // console.log ("invalid input")
+// // // // }
 
-// // let variable =  prompt("Please enter a value");
+// // // let variable =  prompt("Please enter a value");
 
-// // if(isNaN(variable)){
-// //     console.log("Given variale is  not an integer")
+// // // if(isNaN(variable)){
+// // //     console.log("Given variale is  not an integer")
+// // // }
+// // // else {
+// // //     console.log("Given variable is an integer")
+// // // }
+// // var n = 'a'
+// // if(isalpha(n))
+// // {
+// // console.log("it is an alphabet")
 // // }
-// // else {
-// //     console.log("Given variable is an integer")
-// // }
-// var n = 'a'
-// if(isalpha(n))
-// {
-// console.log("it is an alphabet")
-// }
-// else{
-//     console.log("it is not an alphabet")
-// }
-var n1=55
-var n2=56
-var n3="44.3453"
-n1 =n1.toString()
-console.log("sum",n1+n2+n3)
+// // // else{
+// // //     console.log("it is not an alphabet")
+// // // }
+// // var n1=55
+// // var n2=56
+// // var n3="44.3453"
+// // n1 =n1.toString()
+// // console.log("sum",n1+n2+n3)
+// write a program to convert calcius into farenheight test program
+var tempinc = 10
+var tempinf = tempinc*9/5+32
+console.log("tempinf",tempinf)
