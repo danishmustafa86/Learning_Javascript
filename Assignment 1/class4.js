d9ed3c53c585c0b755d66c4669d40d24fc46a4b2
Danish jajja <danishjajja86@gmail.com>
2023-07-19 22:42:26 +0500

    prog

---
+++ Assignment 1/class4.js
@@ -0,0 +1,4 @@
+var n1 = 5
+var n2 = 7
+var sum = n1+n2
+console.log("sum",n1+n2)
--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -10,17 +10,26 @@
 // console.log("n is negative");
 // else
 // console.log("n is equal to zero");
-// write a program to check a character is an alphabet or not
-var ch = 'a'
-if(ch=='a'||ch=='A'||ch=='b'||ch=='B'||ch=='c'||ch=='C'||ch=='d'||ch=='D'||ch=='e'||ch=='E'||ch=='f'||ch=='F'||
-    ch=='g'||ch=='G'||ch=='h'||ch=='H'||ch=='i'||ch=='I'||ch=='j'||ch=='J'||ch=='k'||ch=='K'||ch=='l'||ch=='L'||
-    ch=='m'||ch=='M'||ch=='n'||ch=='N'||ch=='o'||ch=='O'||ch=='p'||ch=='P'||ch=='q'||ch=='Q'||ch=='r'||ch=='R'||
-    ch=='s'||ch=='S'||ch=='t'||ch=='T'||ch=='u'||ch=='U'||ch=='v'||ch=='V'||ch=='w'||ch=='W'||ch=='x'||ch=='X'||
-    ch=='y'||ch=='Y'||ch=='z'||ch=='Z')
-    {
-        console.log("given character is an alphabet")
-    }
-    else
-    {
-    console.log("it is not an alphabet")
-    }
+// // write a program to check a character is an alphabet or not
+// var ch = 'a'
+// if(ch=='a'||ch=='A'||ch=='b'||ch=='B'||ch=='c'||ch=='C'||ch=='d'||ch=='D'||ch=='e'||ch=='E'||ch=='f'||ch=='F'||
+//     ch=='g'||ch=='G'||ch=='h'||ch=='H'||ch=='i'||ch=='I'||ch=='j'||ch=='J'||ch=='k'||ch=='K'||ch=='l'||ch=='L'||
+//     ch=='m'||ch=='M'||ch=='n'||ch=='N'||ch=='o'||ch=='O'||ch=='p'||ch=='P'||ch=='q'||ch=='Q'||ch=='r'||ch=='R'||
+//     ch=='s'||ch=='S'||ch=='t'||ch=='T'||ch=='u'||ch=='U'||ch=='v'||ch=='V'||ch=='w'||ch=='W'||ch=='x'||ch=='X'||
+//     ch=='y'||ch=='Y'||ch=='z'||ch=='Z')
+//     {
+//         console.log("given character is an alphabet")
+//     }
+//     else
+//     {
+//     console.log("it is not an alphabet")
+//     }
+var n = 3
+if(isNaN(n))
+{
+console.log("it is not a number")
+}
+else
+{
+    console.log("it is a number")
+}
