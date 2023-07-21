31f03267b8011db4cb8247a73519540b2d6bffdd
Danish jajja <danishjajja86@gmail.com>
2023-07-21 16:45:22 +0500

    true

--- Assignment 1/class no 4/s.js
+++ Assignment 1/class no 4/s.js
@@ -1,4 +1,3 @@
 'flavio'.length //6
 const name = 'flavio'
 name.length//6
-console.log("length property")
--- Assignment 1/class4.js
+++ Assignment 1/class4.js
@@ -1,4 +1,6 @@
 var n1 = 5
-var n2 = 7
-var sum = n1+n2
-console.log("sum",n1+n2)
+var n2 = 5
+var n3 = 4
+var n4 = 7
+var sum = n1>=n2||n1>n4&&n3<=n1||n3<=n2&&n1==n3
+console.log("su",sum)
