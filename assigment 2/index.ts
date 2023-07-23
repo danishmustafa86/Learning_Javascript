5d0620b15396f0c0da85d553df4b39315d19fc53
Danish jajja <danishjajja86@gmail.com>
2023-07-23 22:42:49 +0500

    lear

--- assigment 2/index.ts
+++ assigment 2/index.ts
@@ -1,9 +1,8 @@
-"use strict";
 // var n1:number=8
 // var n2:number=3
 // var sum:number=n1+n2
 // console.log(sum)
-var n1 = 3;
-var n2 = 8;
-var sum = n1 + n2;
-console.log(sum);
+var n1: number = 3
+var n2: number = 8
+var sum: number = n1 + n2
+console.log(sum)
