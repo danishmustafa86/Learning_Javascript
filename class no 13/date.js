b7aa74830f903f81e766246aa0e9ee7f595b1d4f
Danish jajja <danishjajja86@gmail.com>
2023-07-25 23:29:10 +0500

    date learning in javascript

---
+++ class no 13/date.js
@@ -0,0 +1,21 @@
+var pastDate = "2021-02-15T15:22:09.222"
+var future = "2023-08-23T15:02:09.345"
+
+var currentDate = new Date(future)
+console.log("curentDate",currentDate)
+var year = currentDate.getFullYear()
+var month = currentDate.getMonth()
+var date = currentDate.getDate()
+var hour = currentDate.getHours()
+var minut = currentDate.getMinutes()
+var time = currentDate.getTime()
+var getDays = currentDate.getDay()
+console.log("year",year)
+console.log("month",month)
+console.log("date",date)
+console.log("hour",hour)
+console.log("minut",minut)
+console.log("time",time)
+console.log("day",getDays)
+var mont = ['jan','feb','mar','apr','may','june','july','aug']
+console.log("months",mont[month])
