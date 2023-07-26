4ced350928731a66aa1f581c2cc8a493638316d8
Danish jajja <danishjajja86@gmail.com>
2023-07-26 20:44:22 +0500

    pro 10 assigment 2

--- assigment 2/pro.js
+++ assigment 2/pro.js
@@ -256,7 +256,7 @@
 
 // write a program to check a number is even or odd in switch case
 
-// 
+//
 
 //  Write a js program to find roots of a quadratic equation using switch case.
 
@@ -280,21 +280,79 @@
 
 // Write a js program to input any character and check whether it is alphabet, digit or special character.
 
+// var ch='?'
+// if(ch=='a'||ch=='b'||ch=='c'||ch=='d'||ch=='e'||ch=='f'||ch=='g'||ch =='h'||ch=='i'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'
+// ||ch=='o'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='u'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z'){
+//     console.log("it is alphabet")
+// }
+// else if(ch==1||ch==2||ch==3||ch==4||ch==5||ch==6||ch==7||ch==8||ch==9||ch==0){
+//     console.log("it is digit")
+// }
+// else{
+//     console.log("it is special character")
+// }
 
-var ch='?'
-if(ch=='a'||ch=='b'||ch=='c'||ch=='d'||ch=='e'||ch=='f'||ch=='g'||ch =='h'||ch=='i'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'
-||ch=='o'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='u'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z'){
-    console.log("it is alphabet")
+// Write a js program to check whether a character is uppercase or lowercase alphabet.
+
+var ch = 'z'
+if (
+  ch == 'a' ||
+  ch == 'b' ||
+  ch == 'c' ||
+  ch == 'd' ||
+  ch == 'e' ||
+  ch == 'f' ||
+  ch == 'g' ||
+  ch == 'h' ||
+  ch == 'i' ||
+  ch == 'j' ||
+  ch == 'k' ||
+  ch == 'l' ||
+  ch == 'm' ||
+  ch == 'n' ||
+  ch == 'o' ||
+  ch == 'p' ||
+  ch == 'q' ||
+  ch == 'r' ||
+  ch == 's' ||
+  ch == 't' ||
+  ch == 'u' ||
+  ch == 'v' ||
+  ch == 'w' ||
+  ch == 'x' ||
+  ch == 'y' ||
+  ch == 'z'
+) {
+  console.log('it is lower case')
+} else if (
+  ch == 'A' ||
+  ch == 'B' ||
+  ch == 'C' ||
+  ch == 'D' ||
+  ch == 'E' ||
+  ch == 'F' ||
+  ch == 'G' ||
+  ch == 'H' ||
+  ch == 'I' ||
+  ch == 'J' ||
+  ch == 'K' ||
+  ch == 'L' ||
+  ch == 'M' ||
+  ch == 'N' ||
+  ch == 'O' ||
+  ch == 'P' ||
+  ch == 'Q' ||
+  ch == 'R' ||
+  ch == 'S' ||
+  ch == 'T' ||
+  ch == 'U' ||
+  ch == 'V' ||
+  ch == 'W' ||
+  ch == 'X' ||
+  ch == 'Y' ||
+  ch == 'Z'
+) {
+  console.log('it is UPPER case')
+} else {
+  console.log('invalid input')
 }
-else if(ch==1||ch==2||ch==3||ch==4||ch==5||ch==6||ch==7||ch==8||ch==9||ch==0){
-    console.log("it is digit")
-}
-else{
-    console.log("it is special character")
-}
-
-
-
-
-
-
