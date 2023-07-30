011d940d46178f0f4c986cc1bda62d5f209a3dd4
Danish jajja <danishjajja86@gmail.com>
2023-07-30 23:01:31 +0500

    tsc practice

--- assigment 2/index.js
+++ assigment 2/index.js
@@ -3,7 +3,13 @@
 // var n2:number=3
 // var sum:number=n1+n2
 // console.log(sum)
-var n1 = 3;
-var n2 = 8;
-var sum = n1 + n2;
-console.log(sum);
+// var n1: number = 3
+// var n2: number = 8
+// var su: number = n1 + n2
+// console.log(su)
+// var userName1:string ="ahmad"
+// var userName:string =`Ali${userName1}`
+// console.log(typeof userName)
+var userName1 = "ahmad";
+var userName2 = `Ali`;
+console.log(`"DANISH",ali,${userName1}`);
