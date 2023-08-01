1fdd41e271606e327cae4b613a0e65185040251b
Danish jajja <danishjajja86@gmail.com>
2023-08-01 11:58:29 +0500

    pro arry

--- assigment 2/lrn.js
+++ assigment 2/lrn.js
@@ -1,5 +1,11 @@
-"use strict";
-var n1 = 4;
-var n2 = 5;
-var sum = n1 + n2;
-console.log("sum=>", sum);
+// var n1 = 4;
+// var n2 = 5;
+// var sum = n1 + n2;
+// console.log("sum=>", sum);
+
+let ar=[1,2,3,4]
+let find =ar.find(( elment,index,arry) => {
+    console.log(element, index, arry)
+    return element==2;
+})
+console.log(find);
