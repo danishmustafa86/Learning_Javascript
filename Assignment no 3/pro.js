4d494a5b701b8ab18cc277a0affb001d4ec7c2b0
Danish jajja <danishjajja86@gmail.com>
2023-08-02 17:33:06 +0500

    pro 1 a3

--- Assignment no 3/pro.js
+++ Assignment no 3/pro.js
@@ -1,4 +1,18 @@
 //write a js code to print numbers from 1 to 10
 
-for(let n=1;n<=10;n++){
-console.log(n)}
+// for(let n=1;n<=10;n++){
+// console.log(n)}
+
+//write a program to priont this patern
+//1
+//12
+//123
+//1234
+//12345
+//123456
+//1234567
+//12345678
+
+for(let num=1;num<=8;num++){
+    console.log(num)
+}
