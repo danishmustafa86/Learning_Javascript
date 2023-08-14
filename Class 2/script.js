19bb8107179d2c4a55e272483011149fcea7b2a6
Danish jajja <danishjajja86@gmail.com>
2023-08-14 20:12:02 +0500

    pro

--- Class 2/script.js
+++ Class 2/script.js
@@ -1,16 +1,1 @@
-// practice for variable
-// = means assignment operator
-// keywords 
-var age = 20
-var height = 5.5
-var name = "naveed sarwar"
-//HOW TO GET these variables from RAM?
-console.log(age);
-console.log(height)
-  console.log(name)
-  //write code to calculate 2+2
-  var num1 = 2
-  var num2 = 2
-  var result = num1 + num2
-  console.log(result)
-  
+
